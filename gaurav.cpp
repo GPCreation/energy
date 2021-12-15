dc836f8d0261a1411cda5079aa68d4ea25172614
GPCreation <94772066+GPCreation@users.noreply.github.com>
2021-12-15 13:56:08 +0530

    Add files via upload

---
+++ gaurav.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f;
+    cout<<"programme of arithmatic operation";
+a=5;
+b=6;
+c=a+b;
+cout<<"sum of a+b="<<c;
+d=a-b;
+cout<<"diffrence of a-b="<<d;
+e=a*b;
+cout<<"product of a*b="<<e;
+f=a/b;
+cout<<"divide of a/b="<<f;
+return 0;
+
+}
+    
